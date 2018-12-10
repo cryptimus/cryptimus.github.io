681494fa51eea2c4a766fd6c72223085e803a49d
Saurav <jha15saurav@gmail.com>
2018-12-10 15:46:23 +0530

    theme

--- script/script1.js
+++ script/script1.js
@@ -1,16 +1,17 @@
-var theme=0;
-function toggle() {
-	if(theme==0){
-		document.getElementById('pagestyle').setAttribute('href', "css/styleinvert.css");
-		theme=1;
-	}
-	else{
-		document.getElementById('pagestyle').setAttribute('href', "css/style.css");
-		theme=0;
-	}
-				
-}	
-		
+
+$(document).ready(function(){
+    $("#btn").click(function(){
+		$(".college, .skillicon").toggleClass("invertfilter");
+		$("body").toggleClass("invertbody");
+		$("hr").toggleClass("inverthr");
+		$(".theme").toggleClass("inverttheme");
+		$(".overlay").toggleClass("invertoverlay");
+		$(".btn").toggleClass("invertbtn");
+		$(".contact").toggleClass("invertcontact");
+		$(".contact-icons").toggleClass("invertcontact-icons");
+    });
+});
+
 		
 window.onscroll = function() {scrollFunction()};
 
